db731108dcd0f178457afcc90d1a23c671a87763
Minji Lee <mingg9688@gmail.com>
2019-10-29 14:07:47 +0900

    modified to modern ogl

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,4 @@
 #define GL_SILENCE_DEPRECATION
-
-#include "../include/camera.h"
-// #include "../include/tree.h"
-
-// https://github.com/tomdalling/opengl-series/blob/master/source/04_camera/source/tdogl/Camera.h
-
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -13,146 +7,26 @@
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
-GLFWwindow* window;
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <common/shader.hpp>
+
+#include "../include/camera.h"
+#include "../include/tree.h"
+
+using namespace std;
 using namespace glm;
 
-#include <common/shader.hpp>
+GLFWwindow* window;
 
 static Camera camera;
 
-// static unsigned int width = 700;
-// static unsigned int height = 700;
+int frame = 0;
 
-// static bool mouseRotatePressed = false;
-// static bool mouseMovePressed   = false;
-// static bool mouseZoomPressed   = false;
-// static int lastX = 0, lastY = 0, lastZoom = 0;
-
-// static bool fullScreen = false;
-
-// static Tree* human;
-// int frame = 0;
-
-// void reshape(int w, int h)
-// {
-// 	camera.resize(w, h);
-// }
-
-// void move(int millisec) {
-//     if (frame < human->motionData.num_frames) {
-//         frame++;
-//         glutTimerFunc(human->motionData.frame_time*1000.0, move, 1);
-//         glutPostRedisplay();
-//     }
-// }
-
-// void display()
-// {
-// 	glLoadIdentity();
-// 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-// 	camera.apply();
-//     glColor3f(0.2, 0.45, 0.6);
-//     glPushMatrix();
-//     human->drawMyHuman(human->getRoot(), frame);
-// //    cout << "frame: " << frame << endl;
-//     glPopMatrix();
-// 	glutSwapBuffers();
-// }
-
-// void keyboardCB(unsigned char keyPressed, int x, int y)
-// {
-// 	switch (keyPressed) {
-// 		case 'f':
-// 			if (fullScreen == true) {
-// 				glutReshapeWindow(width, height);
-// 				fullScreen = false;
-// 			} else {
-// 				glutFullScreen();
-// 				fullScreen = true;
-// 			}
-// 			break;
-// 		case 'q':
-// 			exit(0);
-// 			break;
-// 	}
-// 	glutPostRedisplay();
-// }
-
-// void mouseCB(int button, int state, int x, int y)
-// {
-// 	if (state == GLUT_UP)
-// 	{
-// 		mouseMovePressed   = false;
-// 		mouseRotatePressed = false;
-// 		mouseZoomPressed   = false;
-// 	}
-// 	else
-// 	{
-// 		if (button==GLUT_LEFT_BUTTON && GLUT_ACTIVE_SHIFT==glutGetModifiers())
-// 		{
-// 			lastX = x;
-// 			lastY = y;
-// 			mouseMovePressed   = true;
-// 			mouseRotatePressed = false;
-// 			mouseZoomPressed   = false;
-// 		}
-// 		else if (button==GLUT_LEFT_BUTTON && GLUT_ACTIVE_CTRL==glutGetModifiers())
-// 		{
-// 			lastZoom = y;
-// 			mouseMovePressed   = false;
-// 			mouseRotatePressed = false;
-// 			mouseZoomPressed   = true;
-// 		}
-// 		else if (button==GLUT_LEFT_BUTTON)
-// 		{
-// 			camera.beginRotate(x, y);
-// 			mouseMovePressed   = false;
-// 			mouseRotatePressed = true;
-// 			mouseZoomPressed   = false;
-// 		}
-// 	}
-
-// 	glutPostRedisplay();
-// }
-
-// void motionCB(int x, int y)
-// {
-// 	if (mouseRotatePressed == true)
-// 	{
-// 		camera.rotate(x, y);
-// 	}
-// 	else if (mouseMovePressed == true)
-// 	{
-// 		camera.move((x - lastX) / static_cast<float>(width),
-// 				(lastY - y) / static_cast<float>(height), 0.0);
-// 		lastX = x;
-// 		lastY = y;
-// 	}
-// 	else if (mouseZoomPressed == true)
-// 	{
-// 		camera.zoom(float(y - lastZoom) / height);
-// 		lastZoom = y;
-// 	}
-
-// 	glutPostRedisplay();
-// }
-
-// void manual()
-// {
-// 	std::cout << "==================manual=================" << std::endl;
-// 	std::cout << std::endl;
-// 	std::cout << "   rotate  :  left click & drag" << std::endl;
-// 	std::cout << "    zoom   :  ctrl + left click & drag" << std::endl;
-// 	std::cout << " translate :  shift + left click & drag" << std::endl;
-// 	std::cout << "  'f' key  :  full screen" << std::endl;
-// 	std::cout << std::endl;
-// 	std::cout << "=========================================" << std::endl;
-// }
+double gScrollY = 0;
 
 void updateCamera(GLFWwindow* window, float secondsElapsed) {
-	const float moveSpeed = 2.0;
+	const float moveSpeed = 0.02;
 	if (glfwGetKey(window, 'S')) {
 		camera.offsetPosition(secondsElapsed * moveSpeed * -camera.forward());
 	} else if (glfwGetKey(window, 'W')) {
@@ -169,51 +43,26 @@ void updateCamera(GLFWwindow* window, float secondsElapsed) {
         camera.offsetPosition(secondsElapsed * moveSpeed * glm::vec3(0,1,0));
     }
 
-    //rotate camera based on mouse movement
-    const float mouseSensitivity = 0.1f;
-    double mouseX, mouseY;
-    glfwGetCursorPos(window, &mouseX, &mouseY);
-    camera.offsetOrientation(mouseSensitivity * (float)mouseY, mouseSensitivity * (float)mouseX);
-    glfwSetCursorPos(window, 0, 0); //reset the mouse, so it doesn't go out of the window
-
-    // //increase or decrease field of view based on mouse wheel
-    // const float zoomSensitivity = -0.2f;
-    // float fieldOfView = camera.fieldOfView() + zoomSensitivity * (float)gScrollY;
-    // if(fieldOfView < 5.0f) fieldOfView = 5.0f;
-    // if(fieldOfView > 130.0f) fieldOfView = 130.0f;
-    // camera.setFieldOfView(fieldOfView);
-    // gScrollY = 0;
-}
-
-static const GLfloat g_vertex_triangle[] = {
-	-1.0f, -1.0f, 2.0f,
-	1.0f, -1.0f, 2.0f,
-	1.0f, -1.0f, 1.0f
-};
-
-GLuint setUpObjects() {
-	GLuint VertexArrayID;
-	glGenVertexArrays(1, &VertexArrayID);
-	glBindVertexArray(VertexArrayID);
-
-	GLuint vertexbuffer;
-	glGenBuffers(1, &vertexbuffer);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_triangle), g_vertex_triangle, GL_STATIC_DRAW);
-	return vertexbuffer;
+    //increase or decrease field of view based on mouse wheel
+    const float zoomSensitivity = -0.2f;
+    float fieldOfView = camera.fieldOfView() + zoomSensitivity * (float)gScrollY;
+    if(fieldOfView < 5.0f) fieldOfView = 5.0f;
+    if(fieldOfView > 130.0f) fieldOfView = 130.0f;
+    camera.setFieldOfView(fieldOfView);
+    gScrollY = 0;
 }
 
 void drawTriangle(GLuint vertexBuffer) {
 	glEnableVertexAttribArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);	
+	glDrawArrays(GL_TRIANGLES, 0, 12*3);
 
 	glDisableVertexAttribArray(0);
 }
 
-int main( void)
-{
+int setUpGl() {
+	
 	// Initialise GLFW
 	if( !glfwInit() )
 	{
@@ -247,69 +96,65 @@ int main( void)
 		return -1;
 	}
 
-	// Ensure we can capture the escape key being pressed below
-	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	// // Ensure we can capture the escape key being pressed below
+	// glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	// glfwSetScrollCallback(window, OnScroll);
 
-	// Dark blue background
-	glClearColor(0.0f, 0.0f, 0.4f, 0.0f);
-
-	// Enable depth test
+	glClearColor(1.0f, 1.0f, 1.4f, 1.0f);
 	glEnable(GL_DEPTH_TEST);
 	// Accept fragment if it closer to the camera than the former one
 	glDepthFunc(GL_LESS); 
 
-	// Create and compile our GLSL program from the shaders
+}
+
+int main( void)
+{
+	int er = setUpGl();
+	if (er == -1) {
+		cout << "error!" << endl;
+	}
+
+	const string filename = "../MotionData/Trial001.bvh";
+	std::unique_ptr<Tree> human = std::make_unique<Tree>(filename);
+	// TODO: set objects
 	GLuint programID = LoadShaders( "../src/TransformVertexShader.vertexshader", "../src/ColorFragmentShader.fragmentshader" );
-
-	// Get a handle for our "MVP" uniform
 	GLuint MatrixID = glGetUniformLocation(programID, "MVP");
-	// Projection matrix : 45� Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-	glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 3.0f, 0.1f, 100.0f);
 
-	GLuint vertexBuffer;
-	vertexBuffer = setUpObjects();
+	double lastTime = glfwGetTime();
 	do{
-		updateCamera(window);
-		// Camera matrix
-		glm::mat4 View       = glm::lookAt(
-									glm::vec3(4,3,-3), // Camera is at (4,3,-3), in World Space
-									glm::vec3(0,0,0), // and looks at the origin
-									glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
-							);
+		double thisTime = glfwGetTime();
+		updateCamera(window, (float)(thisTime-lastTime));
+
 		// Model matrix : an identity matrix (model will be at the origin)
 		glm::mat4 Model      = glm::mat4(1.0f);
-		// Our ModelViewProjection : multiplication of our 3 matrices
-		glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+		glm::mat4 MVP        = camera.matrix() * Model; // Remember, matrix multiplication is the other way around
 
 		// Clear the screen
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		// Use our shader
 		glUseProgram(programID);
-
-		// Send our transformation to the currently bound shader, 
-		// in the "MVP" uniform
 		glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
 
-		drawTriangle(vertexBuffer);
+    	human->drawMyHuman(human->getRoot(), frame, MVP);
+		frame += 1;
 
 		// Swap buffers
 		glfwSwapBuffers(window);
 		glfwPollEvents();
+
+		// TODO: sleep
 
 	} // Check if the ESC key was pressed or the window was closed
 	while( glfwGetKey(window, GLFW_KEY_ESCAPE ) != GLFW_PRESS &&
 		   glfwWindowShouldClose(window) == 0 );
 
 	// Cleanup VBO and shader
-	glDeleteBuffers(1, &vertexBuffer);
-	// glDeleteBuffers(1, &colorbuffer);
 	glDeleteProgram(programID);
-	// glDeleteVertexArrays(1, &VertexArrayID);
-
-	// Close OpenGL window and terminate GLFW
 	glfwTerminate();
 
 	return 0;
 }
+
+// TODO: visualization of each node
+// TODO: modify Model matrix with the returned values
