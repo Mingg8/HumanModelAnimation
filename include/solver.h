f7e7e2e0a8962d114f29c6b871005509212a3354
Minji Lee <mingg9688@gmail.com>
2019-10-31 21:41:19 +0900

    jacobian function structure

--- include/solver.h
+++ include/solver.h
@@ -1,2 +1,6 @@
 #include <Eigen/Dense>
+#include <tree.h>
 
+using namespace Eigen;
+
+MatrixXd calculateJacobian(Tree& tree);
--- src/solver.cpp
+++ src/solver.cpp
@@ -1,5 +1,34 @@
 #include "../include/solver.h"
 
-static float gradientFuncLS(VectorXd const &d, VectorXd &dp) {
+// TODO: compute jacobian matrix & pseudo-inverse
+//      specify goal position and orientation of a limb (maybe right mouse input)
+//      implement Euler integration
+//      You may leave the skeletal root fixed
 
+MatrixXd calculateJacobian(const Tree& tree, Joint *joint) {
+    MatrixXd jacob(6, tree.motionData.num_motion_channels);
+    Joint *tmpJoint = joint;
+
+    // compute Rsb
+    Matrix3d Rsb;
+    Rsb.setIdentity();
+    while (true) {
+        Matrix3d mat;
+        mat = 
+
+        Rsb = mat * Rsb;
+        if (tmpJoint->getParent() == nullptr) break;
+        else tmpJoint = tmpJoint->getParent();
+    }
+
+    tmpJoint = joint;
+
+    while (true) {
+        // fill jacob for the angle of the tmpJoint
+
+
+        if (tmpJoint->getParent() == nullptr) break;
+        else tmpJoint = tmpJoint->getParent();
+    }
+    return jacob;
 }
